415762dfb23490ab71a6898a25a3761d1e231c67
Ghada Fahad <gasker@tetco.sa>
2020-12-22 20:31:01 +0300

    Setting up webpack

--- src/server/index.js
+++ src/server/index.js
@@ -9,13 +9,17 @@ const mockAPIResponse = require("./mockAPI.js");
 
 const app = express();
 
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.static("dist"));
 
 console.log(__dirname);
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
+  // res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
