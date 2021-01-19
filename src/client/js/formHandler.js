ef40da6f8e1e33673751949cd08b399a529fb892
Ghada Fahad <gasker@tetco.sa>
2021-01-19 15:24:43 +0300

    Change data validation to be in formHandler function not in HTML

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,32 +4,42 @@ function handleSubmit(event) {
   let formText = document.getElementById("feelings-input").value;
   // clean up the text
   formText = formText.trim().toLowerCase();
+  const pattern = /([\/\\0-9().!@#%^&*?><"':|~,_-])+/;
+  const checkFlag = pattern.test(formText);
 
-  // Fetch api
-  const axios = require("axios");
-  axios
-    .get("http://localhost:8080/feelings?txt=" + formText)
-    .then((response) => {
-      // in case of success (200)
-      app.generateFeelings(response.data);
-    })
-    .catch((error) => {
-      // in case of failure
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        document.getElementById("error-msg").innerHTML =
-          error.response.status + " (" + error.response.statusText + ")";
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        document.getElementById("error-msg").innerHTML = error.message;
-      }
-    });
+  if (checkFlag) {
+    //found error, then show an error msg
+    const errorMSG = document.getElementById("error-msg");
+    errorMSG.style = "display: block";
+    errorMSG.innerHTML =
+      "Use words only without numbers or any special character.";
+  } else {
+    // Fetch api
+    const axios = require("axios");
+    axios
+      .get("http://localhost:8080/feelings?txt=" + formText)
+      .then((response) => {
+        // in case of success (200)
+        app.generateFeelings(response.data);
+      })
+      .catch((error) => {
+        // in case of failure
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          document.getElementById("error-msg").innerHTML =
+            error.response.status + " (" + error.response.statusText + ")";
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          document.getElementById("error-msg").innerHTML = error.message;
+        }
+      });
+  }
 }
 
 export { handleSubmit };
